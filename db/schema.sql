3a0331c5b983345c75c307205694e2526aab0c57
Richard Song <rsong1993@gmail.com>
2023-12-19 12:53:18 -0800

    Updated error handling for tables in schema. Addded INT to id

--- db/schema.sql
+++ db/schema.sql
@@ -3,22 +3,22 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE department (
+CREATE TABLE IF NOT EXISTS department (
     id INT PRIMARY KEY,
     name VARCHAR(30),
-)
+);
 
-CREATE TABLE role (
+CREATE TABLE IF NOT EXISTS role (
     id INT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-)
+);
 
-CREATE TABLE employee (
-    id PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS employee (
+    id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id, INT
-)
+);
