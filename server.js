9fa3db0dd219d202366f578af6486c1ef72038f4
Richard Song <rsong1993@gmail.com>
2023-12-19 11:52:42 -0800

    Updated dotenv import with (). Confirmed successfully connection with mySQL

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
-dotenv.config;
+dotenv.config();
 
 
 async function connectToDatabase() {
@@ -11,10 +11,12 @@ async function connectToDatabase() {
             port: process.env.DB_PORT,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
+            // database: process.env.DB_DATABASE,
         })
         console.log("Connected to database!")
     } catch (err) {
         console.error('Error connecting to the database:' ,err)
     };
 };
+
+connectToDatabase();
