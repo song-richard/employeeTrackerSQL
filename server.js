6abfd02f14502769a62aa7376567094eec10e8da
Richard Song <rsong1993@gmail.com>
2023-12-19 11:48:33 -0800

    Updated dependencies. Added initial DB connection function

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const inquirer = require('inquirer');
-const mySql = require('mysql2');
+const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config;
 
+
+async function connectToDatabase() {
+    try {
+        await mysql.createConnection({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+        })
+        console.log("Connected to database!")
+    } catch (err) {
+        console.error('Error connecting to the database:' ,err)
+    };
+};
