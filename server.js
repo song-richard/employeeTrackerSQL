917fef96e06b0a3d167448ff553a0075e996878d
Richard Song <rsong1993@gmail.com>
2023-12-19 15:59:36 -0800

    Reorganized server.js structure. Seperated every view/add function into seperate async functions

--- server.js
+++ server.js
@@ -15,83 +15,10 @@ async function connectToDatabase() {
 
         console.log("Connected to database!");
 
-        //Seed 'department' table
-        const departmentQuestions = [
-            {
-                type: 'input',
-                name: 'name',
-                message: 'Enter departnment name',
-            },
-        ];
-        const departmentData = await inquirer.prompt(departmentQuestions);
-        await connection.execute('INSERT INTO department (name) VALUES (?)', [departmentData.name]);
-        console.log('Department data inserted successfully.');
-
-//----------------------------------------//
-        //Seed 'role' table
-        const roleQuestions = [
-            {
-                type: 'input',
-                name: 'title',
-                message: 'Enter role title:',
-            },
-            {
-                type: 'input',
-                name: 'salary',
-                message: 'Enter role salary:',
-            },
-            {
-                type: 'input',
-                name: 'department_id',
-                message: 'Enter department ID for this role:'
-            },
-        ];
-
-        const roleData = await inquirer.prompt(roleQuestions);
-        await connection.execute('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [
-            roleData.title,
-            roleData.salary,
-            roleData.department_id,
-        ]);
-        console.log("Role data inserted successfully");
-
-//----------------------------------------//
-        //Seed 'employee' table
-        const employeeQuestions = [
-            {
-                type: 'input',
-                name: 'first_name',
-                message: 'Enter employee first name:'
-            },
-            {
-                type: 'input',
-                name: 'last_name',
-                message: 'Enter employee last name:'
-            },
-            {
-                type: 'input',
-                name: 'role_id',
-                message: 'Enter role ID for the employee:'
-            },
-            {
-                type: 'input',
-                name: 'manager_id',
-                message: 'Enter manager ID for the employee'
-            },
-        ];
-
-        const employeeData = await inquirer.prompt(employeeQuestions);
-        await connection.execute('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [
-            employeeData.first_name,
-            employeeData.last_name,
-            employeeData.role_id,
-            employeeData.manager_id,
-        ]);
-        console.log("Employee data inserted successfully");
-
-        await connection.end();
+        return connection;
     } catch (err) {
-        console.error('Error connecting to the database:' ,err);
+        console.error('Error connecting to the database:', err);
+        process.exit(1);
     };
 };
 
@@ -125,6 +52,7 @@ async function viewEmployees(connection) {
     };
 };
 
+//Add Department
 async function addDepartment(connection) {
     const departmentQuestions = [
         {
@@ -141,6 +69,40 @@ async function addDepartment(connection) {
         console.log("Department added successfully!");
     } catch (err) {
         console.error('Error adding department:', err);
+    }
+};
+
+//Add Role
+async function addRole(connection) {
+    const roleQuestions = [
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter role title:',
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Enter role salary:',
+        },
+        {
+            type: 'input',
+            name: 'department_id',
+            message: 'Enter department ID for this role:',
+        },
+    ];
+
+    const roleData = await inquirer.prompt(roleQuestions);
+
+    try {
+        await connection.execute('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [
+            roleData.title,
+            roleData.salary,
+            roleData.department_id,
+        ]);
+        console.log("Role added successfully!");
+    } catch (err) {
+        console.error('Error adding role:', err);
     }
 };
 
